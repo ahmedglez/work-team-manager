b26b5ecb736690dc11168dac43284bd43f555a5e
Ahmedglez <ahmediglez@gmail.com>
2022-11-15 17:29:38 -0500

    server running in port 3000

---
+++ index.js
@@ -0,0 +1,33 @@
+/* IMPORTS */
+const dotenv = require("dotenv")
+dotenv.config({
+	path: ".env",
+})
+const express = require("express")
+const cors = require("cors")
+
+/* SERVER CONFIGURATION */
+const app = express()
+const port = process.env.SERVER_PORT || 3000
+app.use(express.json())
+const whitelist = ["http://localhost:8080", "https://myapp.co"]
+const options = {
+	origin: (origin, callback) => {
+		if (whitelist.includes(origin) || !origin) {
+			callback(null, true)
+		} else {
+			callback(new Error("no permitido"))
+		}
+	},
+}
+app.use(cors(options))
+
+/* ROUTES */
+app.get("/", (req, res) => {
+	res.send("Hello World!")
+})
+
+/* SERVER START */
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`)
+})
